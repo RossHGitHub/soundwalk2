0b15bb4b0b2b693fc803aefe11063a52fff4150c
Ross Henderson <rossliamhenderson@gmail.com>
2025-08-20 22:14:27 +0100

    updated imports

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Gigs from "./pages/Gigs";
 import MediaPage from "./pages/Media";
 import ContactPage from "./pages/Contact";
 import Admin from "./pages/Admin";
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import ProtectedRoute from "./components/protectedRoute";
 
 function App() {
